e9317b4ec9b05db8ea1006a9f408c5223d6707e6
Sofie <sofjaslan@gmail.com>
2021-02-09 13:04:09 +0100

    Initial commit

---
+++ exercises/hello/hello.c
@@ -0,0 +1,6 @@
+#include "stdio.h" // for html display only, you should use angular brackets
+#include "math.h"  // for html display only, you should use angular brackets
+int main(void){
+	printf("hello world!\n");
+return 0;
+}
