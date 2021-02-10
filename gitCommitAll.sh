f14616acddaf9481d9add6664c6819191e48d603
Sofie <sofjaslan@gmail.com>
2021-02-10 12:38:25 +0100

    Q2 answers

--- gitCommitAll.sh
+++ gitCommitAll.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Standard simple shell script to quickly commit all changes
+
+echo "Enter your commit text to this push using the format 'sometext': "
+					# This will prompt the user
+read  COMMIT 				# to input the commit text
+
+# This will initiallize, add all changes, commit and push
 git init
 git add --all
-git commit --all -m 'Initial commit'
+git commit --all -m "$COMMIT"
 git push
